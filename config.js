73d1cdd262e5c00d2b0f6ded2bc1c096fcf86adc
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-09 19:06:37 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  proxy: "https://ratp-proxy.hippodrome-proxy42.workers.dev/?url=",
+  stops: {
+    rer: "STIF:StopArea:SP:43135:",
+    bus77: "STIF:StopArea:SP:463641:",
+    bus201: "STIF:StopArea:SP:463644:"
+  }
+};
