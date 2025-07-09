0ced73576ab76c0894f50ef0666ff61fe5c5c0a2
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-09 22:07:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,18 +7,19 @@ export const CONFIG = {
     lon: 2.4638
   },
 newsUrl: "https://api.rss2json.com/v1/api.json?rss_url=https://www.francetvinfo.fr/titres.rss",
-  stops: {
-    rer: {
-      monitoringRef: "43135",
-      lineRef: "STIF:Line::C01742:"
-    },
-    bus77: {
-      monitoringRef: "463641",
-      lineRef: "STIF:Line::C01789:"
-    },
-    bus201: {
-      monitoringRef: "463644",
-      lineRef: "STIF:Line::C01805:"
-    }
+stops: {
+  rer: {
+    monitoringRef: "STIF:StopArea:SP:43135:",
+    lineRef: "STIF:Line::C01742:"
+  },
+  bus77: {
+    monitoringRef: "STIF:StopArea:SP:463641:",
+    lineRef: "STIF:Line::C01789:"
+  },
+  bus201: {
+    monitoringRef: "STIF:StopArea:SP:463644:",
+    lineRef: "STIF:Line::C01805:"
   }
+}
+
 };
