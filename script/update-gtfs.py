c1a82153fb61ee14d07aa5c071efd71dd1b9b67f
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-12 06:21:01 +0200

    Update update-gtfs.py

--- script/update-gtfs.py
+++ script/update-gtfs.py
@@ -9,8 +9,8 @@ GTFS_URL = "https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM-gtfs.zip"
 
 TARGET_LINES = [
     "IDFM:C02251",         # Bus 77
-    "IDFM:C01805",         # Bus 201
-    "STIF:Line::C01742:"   # RER A
+    "IDFM:C01219",         # Bus 201
+    "IDFM:C01742"   # RER A
 ]
 
 OUTPUT_FILE = "static/gtfs_firstlast.json"
